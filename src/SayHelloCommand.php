ea688382476cd4c93e3418255ec4b9aed81beda2
Robert-Roy <robertproy@live.com>
2018-06-13 17:03:58 -0500

    FEATURE: Add greeting option to sayHelloTo

--- src/SayHelloCommand.php
+++ src/SayHelloCommand.php
@@ -6,18 +6,21 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class SayHelloCommand extends Command {
 
     public function configure() {
         $this->setName("sayHelloTo")
                 ->setDescription("Offer greeting to given person.")
-                ->addArgument('name', InputArgument::REQUIRED, "Your name.");
+                ->addArgument('name', InputArgument::REQUIRED, "Your name.")
+                ->addOption('greeting', null, InputOption::VALUE_OPTIONAL, "Override the default greeting", "Hello");
     }
 
     public function execute(InputInterface $input, OutputInterface $output) {
         $name = $input->getArgument('name');
-        $message = 'Hello, ' . $name . ".";
+        $greeting = $input->getOption('greeting');
+        $message = $greeting . ', ' . $name . ".";
         $output->writeln("<info>{$message}</info>");
     }
 
